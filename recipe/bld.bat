ec536f1cd1047a30c1c43fec124d2859d3a53b67
Silvio Traversaro <silvio@traversaro.it>
2023-02-15 13:19:01 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,8 @@ set TMPDIR=%cd%\localtmpdir
 set TEMP=%cd%\localtmpdir
 set TMP=%cd%\localtmpdir
 
-# See https://github.com/conda-forge/labmaze-feedstock/pull/4#issuecomment-1431197953
-set CL="/std:c++17"
+set "CC=clang-cl.exe"
+set "CXX=clang-cl.exe"
 
 %PYTHON% -m pip install . -vv --no-deps
 if errorlevel 1 exit 1
