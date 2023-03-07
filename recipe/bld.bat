5171b32dcd7950b2f626bcdade413978b8743de5
Silvio Traversaro <silvio@traversaro.it>
2023-03-07 11:55:33 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,6 @@ set TMPDIR=%cd%\localtmpdir
 set TEMP=%cd%\localtmpdir
 set TMP=%cd%\localtmpdir
 
-set "BAZEL_CXXOPTS=/std=c++17"
-
 %PYTHON% -m pip install . -vv --no-deps
 if errorlevel 1 exit 1
 
